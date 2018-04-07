b59f1cdeaf977c6c2ed6be991fe494e77746c076
Saeed EY <SaeedEY@users.noreply.github.com>
2018-04-07 21:43:08 +0430

    Add files via upload

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.GroupBox();
+            this.tr = new System.Windows.Forms.PictureBox();
+            this.tc = new System.Windows.Forms.PictureBox();
+            this.tl = new System.Windows.Forms.PictureBox();
+            this.cl = new System.Windows.Forms.PictureBox();
+            this.cr = new System.Windows.Forms.PictureBox();
+            this.br = new System.Windows.Forms.PictureBox();
+            this.bc = new System.Windows.Forms.PictureBox();
+            this.cc = new System.Windows.Forms.PictureBox();
+            this.bl = new System.Windows.Forms.PictureBox();
+            this.table.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.br)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.Controls.Add(this.tr);
+            this.table.Controls.Add(this.tc);
+            this.table.Controls.Add(this.tl);
+            this.table.Controls.Add(this.cl);
+            this.table.Controls.Add(this.cr);
+            this.table.Controls.Add(this.br);
+            this.table.Controls.Add(this.bc);
+            this.table.Controls.Add(this.cc);
+            this.table.Controls.Add(this.bl);
+            this.table.Location = new System.Drawing.Point(12, 13);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(200, 210);
+            this.table.TabIndex = 0;
+            this.table.TabStop = false;
+            this.table.Text = "Player";
+            // 
+            // tr
+            // 
+            this.tr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tr.Location = new System.Drawing.Point(131, 14);
+            this.tr.Name = "tr";
+            this.tr.Size = new System.Drawing.Size(64, 64);
+            this.tr.TabIndex = 7;
+            this.tr.TabStop = false;
+            this.tr.Click += new System.EventHandler(this.tr_Click);
+            // 
+            // tc
+            // 
+            this.tc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tc.Location = new System.Drawing.Point(68, 14);
+            this.tc.Name = "tc";
+            this.tc.Size = new System.Drawing.Size(64, 64);
+            this.tc.TabIndex = 1;
+            this.tc.TabStop = false;
+            this.tc.Click += new System.EventHandler(this.tc_Click);
+            // 
+            // tl
+            // 
+            this.tl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tl.Location = new System.Drawing.Point(5, 14);
+            this.tl.Name = "tl";
+            this.tl.Size = new System.Drawing.Size(64, 64);
+            this.tl.TabIndex = 6;
+            this.tl.TabStop = false;
+            this.tl.Click += new System.EventHandler(this.tl_Click);
+            // 
+            // cl
+            // 
+            this.cl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cl.Location = new System.Drawing.Point(5, 77);
+            this.cl.Name = "cl";
+            this.cl.Size = new System.Drawing.Size(64, 64);
+            this.cl.TabIndex = 5;
+            this.cl.TabStop = false;
+            this.cl.Click += new System.EventHandler(this.cl_Click);
+            // 
+            // cr
+            // 
+            this.cr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cr.Location = new System.Drawing.Point(131, 77);
+            this.cr.Name = "cr";
+            this.cr.Size = new System.Drawing.Size(64, 64);
+            this.cr.TabIndex = 4;
+            this.cr.TabStop = false;
+            this.cr.Click += new System.EventHandler(this.cr_Click);
+            // 
+            // br
+            // 
+            this.br.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.br.Location = new System.Drawing.Point(131, 140);
+            this.br.Name = "br";
+            this.br.Size = new System.Drawing.Size(64, 64);
+            this.br.TabIndex = 3;
+            this.br.TabStop = false;
+            this.br.Click += new System.EventHandler(this.br_Click);
+            // 
+            // bc
+            // 
+            this.bc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bc.Location = new System.Drawing.Point(68, 140);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(64, 64);
+            this.bc.TabIndex = 2;
+            this.bc.TabStop = false;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // cc
+            // 
+            this.cc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cc.Location = new System.Drawing.Point(68, 77);
+            this.cc.Name = "cc";
+            this.cc.Size = new System.Drawing.Size(64, 64);
+            this.cc.TabIndex = 1;
+            this.cc.TabStop = false;
+            this.cc.Click += new System.EventHandler(this.cc_Click);
+            // 
+            // bl
+            // 
+            this.bl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bl.Location = new System.Drawing.Point(5, 140);
+            this.bl.Name = "bl";
+            this.bl.Size = new System.Drawing.Size(64, 64);
+            this.bl.TabIndex = 0;
+            this.bl.TabStop = false;
+            this.bl.Click += new System.EventHandler(this.bl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 232);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.table.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.br)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox table;
+        private System.Windows.Forms.PictureBox tr;
+        private System.Windows.Forms.PictureBox tc;
+        private System.Windows.Forms.PictureBox tl;
+        private System.Windows.Forms.PictureBox cl;
+        private System.Windows.Forms.PictureBox cr;
+        private System.Windows.Forms.PictureBox br;
+        private System.Windows.Forms.PictureBox bc;
+        private System.Windows.Forms.PictureBox cc;
+        private System.Windows.Forms.PictureBox bl;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        private bool myMove = true;
+        private int[] box;
+        private int[][] moves;
+        private int PLAYER_1 = -1;
+        private int PLAYER_2 = -2;
+        private int DIFFICULITY = 1;//1=Easy - 2=Normal - 3=Hard
+        private PictureBox[] pbox;
+        private bool checkWinAlgorithm(int[] indexes)
+        {
+            if (indexes.Length <= 2)
+                return false;
+            if (indexes.Contains(8))
+            {
+                if (indexes.Contains(4) && indexes.Contains(0))
+                    return true;
+                else if (indexes.Contains(7) && indexes.Contains(6))
+                    return true;
+                else if (indexes.Contains(5) && indexes.Contains(2))
+                    return true;
+            }
+            if (indexes.Contains(6))
+            {
+                if (indexes.Contains(3) && indexes.Contains(0))
+                    return true;
+                else if (indexes.Contains(4) && indexes.Contains(2))
+                    return true;
+            }
+            if (indexes.Contains(0))
+            {
+                if (indexes.Contains(1) && indexes.Contains(2))
+                    return true;
+            }
+            if (indexes.Contains(4))
+            {
+                if (indexes.Contains(7) && indexes.Contains(1))
+                    return true;
+                else if (indexes.Contains(5) && indexes.Contains(3))
+                    return true;
+            }
+            return false;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] emptyBoxes()
+        {
+            int len = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (box[i] > 0)
+                    len += 1;
+            }
+            int[] eB = new int[len];
+            int index = 0;
+            for(int i = 0; i < 9; i++)
+            {
+                if(box[i] >= 0)
+                {
+                    if (index >= eB.Length)
+                        break;
+                    eB[index] = i;
+                    index += 1;
+                }
+            }
+            return eB;
+        }
+        public int[] playerXboxes(int PLAYER)
+        {
+            int len = 0;
+            foreach(int a in box)
+            {
+                if (a == PLAYER)
+                    len += 1;
+            }
+            int[] new_arr = new int[len];
+            int index = 0;
+            for(int i = 0; i < 9; i++)
+            {
+                if(box[i] == PLAYER)
+                {
+                    new_arr[index] = i;
+                    index += 1;
+                }
+            }
+            return new_arr;
+        }
+        public bool hasWinner(int PLAYER)
+        {
+            return checkWinAlgorithm(playerXboxes(PLAYER));
+        }
+        
+        public void pcTurn()
+        {
+            int box_number=0;
+            table.Enabled = true;
+            myMove = true;
+            doDecision(ref box_number);
+            box[box_number] = PLAYER_2;
+            pbox[box_number].ImageLocation = "./o.png";
+            if (hasWinner(PLAYER_2))
+            {
+                table.Enabled = false;
+                DialogResult dr = MessageBox.Show("Do you want to Play again?", "You Lose", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                    Application.Restart();
+                else
+                    Application.Exit();
+            }
+        }
+        public void myTurn(int box_number)
+        {
+            if (box[box_number] >= 0)
+            {
+                box[box_number] = PLAYER_1;
+                pbox[box_number].ImageLocation = "./x.png";
+                table.Enabled = false;
+                myMove = false;
+                if (!hasWinner(PLAYER_1))
+                    play();
+                else
+                {
+                    table.Enabled = false;
+                    DialogResult dr = MessageBox.Show("Do you want to Play again?", "You Win",MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                        Application.Restart();
+                    else
+                        Application.Exit();
+                }
+            }
+            
+        }
+        public void doDecision(ref int box_number)
+        {
+            if(DIFFICULITY == 1)
+            {
+                int[] i = emptyBoxes();
+                Random rnd = new Random();
+                int random = (int)(rnd.Next(0, i.Length));
+                try
+                {
+                    box_number = i[random];
+                }
+                catch
+                {
+                    if (i.Length == 0)
+                    {
+                        DialogResult dr = MessageBox.Show("Do you want to Play again?", "Equivalence !", MessageBoxButtons.YesNo);
+                        if (dr == DialogResult.Yes)
+                            Application.Restart();
+                        else
+                            Application.Exit();
+                    }
+                }
+            }else if(DIFFICULITY == 2)
+            {
+
+            }
+            
+            
+
+        }
+        public void play(int box_number=-1)
+        {
+            if (myMove)
+                myTurn(box_number);
+            else
+                pcTurn();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            box = new int[9];
+            pbox = new PictureBox[9];
+            moves = new int[2][];
+            for(int i = 0; i < 9; i++)
+                box[i] = i;
+            moves[0] = new int[5];
+            moves[1] = new int[5];
+            pbox[0] = tl;
+            pbox[1] = tc;
+            pbox[2] = tr;
+            pbox[3] = cl;
+            pbox[4] = cc;
+            pbox[5] = cr;
+            pbox[6] = bl;
+            pbox[7] = bc;
+            pbox[8] = br;
+        }
+        private void tl_Click(object sender, EventArgs e)
+        {
+            play(0);
+        }
+        private void tc_Click(object sender, EventArgs e)
+        {
+            play(1);
+        }
+        private void tr_Click(object sender, EventArgs e)
+        {
+            play(2);
+        }
+        private void cl_Click(object sender, EventArgs e)
+        {
+            play(3);
+        }
+        private void cc_Click(object sender, EventArgs e)
+        {
+            play(4);
+        }
+        private void cr_Click(object sender, EventArgs e)
+        {
+            play(5);
+        }
+        private void bl_Click(object sender, EventArgs e)
+        {
+            play(6);
+        }
+        private void bc_Click(object sender, EventArgs e)
+        {
+            play(7);
+        }
+        private void br_Click(object sender, EventArgs e)
+        {
+            play(8);
+        }
+    }
+}
